84742c479b061b1c51d54ed78781313f336a068a
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-05-14 18:47:58 +0900

    feat: (redux) product API 클라이언트 생성

---
+++ coz-shopping/app/redux/productApi.ts
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+export const productApi = createApi({
+  reducerPath: "productApi",
+  refetchOnFocus: true,
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://cozshopping.codestates-seb.link/api/v1/",
+  }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => "products",
+    }),
+    getProductByCount: builder.query ({
+      query:  counter  => `products?count=${counter}`,
+    }),
+  }),
+});
+
+export const { useGetProductsQuery, useGetProductByCountQuery } = productApi;
