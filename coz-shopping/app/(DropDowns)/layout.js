def7228f6a8fe4be2480e735c70da6686163d6a1
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-05-17 13:55:27 +0900

    refac: enum으로 데이터 관리

--- coz-shopping/app/(DropDowns)/layout.js
+++ coz-shopping/app/(DropDowns)/layout.js
@@ -3,6 +3,13 @@
 // import Image from "next/image"
 import styled from "styled-components";
 import { useState } from "react";
+import React from 'react'
+
+const 전체 = '전체'
+const 상품 = '상품'
+const 카테고리 = '카테고리'
+const 기획전 = '기획전'
+const 브랜드 = '브랜드'
 
 const StyledImage = styled.img`
   transition: transform 0.3s;
@@ -40,17 +47,12 @@ border-bottom: 2px solid transparent;
 @media (max-width: 400px) {
   display: none;
   }
-
-
 `;
 
 const ImageWrapper = styled.div`
   overflow: hidden;
   border-radius: 50%;
 `;
-
-
-
 
 
 export default function Category({ children }) {
@@ -75,33 +77,44 @@ export default function Category({ children }) {
     <section className=" h-99 ">
       <div className="flex justify-center items-center mt-6 mb-6">
 
-        <MenuBox className={`flex flex-col justify-center items-center ${selectedMenu === '전체' ? 'selected' : ''}`} >
-          <ImageWrapper><StyledImage isHovered={hoveredMenu==='전체' ? 'isHovered' : null} src="/전체.png" width={82} height={82} alt="전체" /></ImageWrapper>
-          <Title onClick={() => handleTitleClick('전체')} onMouseEnter={()=>handleTitleHover('전체')} onMouseLeave={handleTitleLeave}>전체</Title>
+        <MenuBox className={`flex flex-col justify-center items-center ${selectedMenu === 전체 ? 'selected' : ''}`} >
+          <ImageWrapper><StyledImage isHovered={hoveredMenu===전체 ? 'isHovered' : null} src="/전체.png" width={82} height={82} alt="전체" /></ImageWrapper>
+          <Title onClick={() => handleTitleClick(전체)} 
+          onMouseEnter={()=>handleTitleHover(전체)} onMouseLeave={handleTitleLeave}>전체</Title>
         </MenuBox>
 
+        <MenuBox className={`flex flex-col justify-center items-center ${selectedMenu === 상품 ? 'selected' : ''}`}> 
+          <ImageWrapper><StyledImage isHovered={hoveredMenu===상품 ? 'isHovered' : null} src="/상품.png" width={82} height={82} alt="상품" /></ImageWrapper>
+          <Title  onClick={() => handleTitleClick(상품)} 
+          onMouseEnter={()=>handleTitleHover(상품)} onMouseLeave={handleTitleLeave}>상품</Title>   
+        </MenuBox>
 
+        <MenuBox className={`flex flex-col justify-center items-center ${selectedMenu === 카테고리 ? 'selected' : ''}`}> 
+          <ImageWrapper><StyledImage isHovered={hoveredMenu===카테고리 ? 'isHovered' : null} src="/카테고리.png" width={82} height={82} alt="카테고리" /></ImageWrapper>
+          <Title  onClick={() => handleTitleClick(카테고리)} 
+          onMouseEnter={()=>handleTitleHover(카테고리)} onMouseLeave={handleTitleLeave}>카테고리</Title>
+        </MenuBox>
 
+        <MenuBox className={`flex flex-col justify-center items-center ${selectedMenu === 기획전 ? 'selected' : ''}`}> 
+          <ImageWrapper><StyledImage isHovered={hoveredMenu===기획전 ? 'isHovered' : null} src="/기획전.png" width={82} height={82} alt="기획전" /></ImageWrapper>
+          <Title  onClick={() => handleTitleClick(기획전)} 
+          onMouseEnter={()=>handleTitleHover(기획전)} onMouseLeave={handleTitleLeave}>기획전</Title>
+        </MenuBox>
 
-        <MenuBox className={`flex flex-col justify-center items-center ${selectedMenu === '상품' ? 'selected' : ''}`}> 
-          <ImageWrapper><StyledImage isHovered={hoveredMenu==='상품' ? 'isHovered' : null} src="/상품.png" width={82} height={82} alt="상품" /></ImageWrapper>
-          <Title  onClick={() => handleTitleClick('상품')} onMouseEnter={()=>handleTitleHover('상품')} onMouseLeave={handleTitleLeave}>상품</Title>   
+        <MenuBox className={`flex flex-col justify-center items-center ${selectedMenu === 브랜드 ? 'selected' : ''}`}> 
+          <ImageWrapper><StyledImage isHovered={hoveredMenu===브랜드 ? 'isHovered' : null} src="/브랜드.png" width={82} height={82} alt="브랜드" /></ImageWrapper>
+          <Title  onClick={() => handleTitleClick(브랜드)} 
+          onMouseEnter={()=>handleTitleHover(브랜드)} onMouseLeave={handleTitleLeave}>브랜드</Title>
         </MenuBox>
-        <MenuBox className={`flex flex-col justify-center items-center ${selectedMenu === '카테고리' ? 'selected' : ''}`}> 
-          <ImageWrapper><StyledImage isHovered={hoveredMenu==='카테고리' ? 'isHovered' : null} src="/카테고리.png" width={82} height={82} alt="카테고리" /></ImageWrapper>
-          <Title  onClick={() => handleTitleClick('카테고리')} onMouseEnter={()=>handleTitleHover('카테고리')} onMouseLeave={handleTitleLeave}>카테고리</Title>
-        </MenuBox>
-        <MenuBox className={`flex flex-col justify-center items-center ${selectedMenu === '기획전' ? 'selected' : ''}`}> 
-          <ImageWrapper><StyledImage isHovered={hoveredMenu==='기획전' ? 'isHovered' : null} src="/기획전.png" width={82} height={82} alt="기획전" /></ImageWrapper>
-          <Title  onClick={() => handleTitleClick('기획전')} onMouseEnter={()=>handleTitleHover('기획전')} onMouseLeave={handleTitleLeave}>기획전</Title>
-        </MenuBox>
-        <MenuBox className={`flex flex-col justify-center items-center ${selectedMenu === '브랜드' ? 'selected' : ''}`}> 
-          <ImageWrapper><StyledImage isHovered={hoveredMenu==='브랜드' ? 'isHovered' : null} src="/브랜드.png" width={82} height={82} alt="브랜드" /></ImageWrapper>
-          <Title  onClick={() => handleTitleClick('브랜드')} onMouseEnter={()=>handleTitleHover('브랜드')} onMouseLeave={handleTitleLeave}>브랜드</Title>
-        </MenuBox>
+
       </div>
-      <main>{children}</main>
-    </section>
+      <main>
+        {React.Children.map(children, (child) =>
+            React.cloneElement(child, { selectedMenu })
+          )}
+      </main>    
+      </section>
 
   )
 }
+
