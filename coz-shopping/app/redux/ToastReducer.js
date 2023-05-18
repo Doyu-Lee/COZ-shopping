b3b29aaf29b36ced7fe629b2ef8ac66140391dcb
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-05-18 19:54:11 +0900

    feat: (redux) 토스트 컴포넌트 추가 및 삭제 데이터 전역 관리

---
+++ coz-shopping/app/redux/ToastReducer.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { notifications: [] }
+
+
+
+export const toastReducer = createSlice({
+  name: "toastReducer",
+  initialState,
+  reducers: {
+    enqueue: (state, action) => {
+      return {...state, notifications: [...state.notifications, action.payload]};
+    },
+    dequeue: (state) => {
+      return {...state, notifications: state.notifications.slice(1)};
+    },
+  },
+});
+
+
+
+
+
+export const { enqueue, dequeue } = toastReducer.actions;
+export default toastReducer.reducer;
--- coz-shopping/app/redux/store.ts
+++ coz-shopping/app/redux/store.ts
@@ -2,22 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { productApi } from "./productApi";
 import bookMarkedProducts from "./bookmarkReducer"
-// import alarmReducer from "./alarm"
-
-// import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import toastReducer from "./ToastReducer"
 
 export const store = configureStore({
   reducer: {
     bookMarkedProducts,
-    // alarmReducer,
+    toastReducer,
     [productApi.reducerPath]: productApi.reducer,
   },
   devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([productApi.middleware]),
 });
-
-// setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
